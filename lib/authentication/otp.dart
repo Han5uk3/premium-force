73bda4dddbab51bbc4f1188e7ca5562cdd05ce25
Han5uk3 <adnanyousufpangat@gmail.com>
2026-02-11 14:07:59 +0530

    created animated snackbar and converted to reusable widget

--- lib/authentication/otp.dart
+++ lib/authentication/otp.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pinput/pinput.dart';
 import 'package:premium_force_main/common_widgets/button.dart';
+import 'package:premium_force_main/common_widgets/snackbar.dart';
 import 'package:premium_force_main/home/home.dart';
 
 class OTPVerificationPage extends StatefulWidget {
@@ -35,8 +36,9 @@ class _OTPVerificationPageState extends State<OTPVerificationPage> {
         right: 20,
         child: Material(
           color: Colors.transparent,
-          child: _AnimatedSnackBar(
+          child: AnimatedSnackBar(
             message: message,
+            type: "E",
             onDismissed: () {
               if (mounted) {
                 _overlayEntry?.remove();
@@ -189,138 +191,6 @@ class _OTPVerificationPageState extends State<OTPVerificationPage> {
                 },
               ),
             ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _AnimatedSnackBar extends StatefulWidget {
-  final String message;
-  final VoidCallback onDismissed;
-
-  const _AnimatedSnackBar({required this.message, required this.onDismissed});
-
-  @override
-  State<_AnimatedSnackBar> createState() => _AnimatedSnackBarState();
-}
-
-class _AnimatedSnackBarState extends State<_AnimatedSnackBar>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _scaleAnimation;
-  late Animation<double> _fadeAnimation;
-  late Animation<Offset> _slideAnimation;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 600), // Smooth, luxurious duration
-      reverseDuration: const Duration(milliseconds: 400),
-    );
-
-    _slideAnimation =
-        Tween<Offset>(
-          begin: const Offset(0, 0.8), // Gentle slide up
-          end: Offset.zero,
-        ).animate(
-          CurvedAnimation(
-            parent: _controller,
-            curve: Curves.easeOutCubic, // Smooth deceleration, no bounce
-            reverseCurve: Curves.easeInCubic,
-          ),
-        );
-
-    _scaleAnimation = Tween<double>(
-      begin: 0.95, // Subtle scale effect
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeOutCubic));
-
-    // New Fade Animation
-    _fadeAnimation = Tween<double>(
-      begin: 0.0,
-      end: 1.0,
-    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeIn));
-
-    _controller.forward();
-
-    Future.delayed(const Duration(seconds: 3), () {
-      if (mounted) {
-        _controller.reverse().then((_) => widget.onDismissed());
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Dismissible(
-      key: UniqueKey(),
-      direction: DismissDirection.down,
-      onDismissed: (_) => widget.onDismissed(),
-      child: FadeTransition(
-        opacity: _fadeAnimation,
-        child: SlideTransition(
-          position: _slideAnimation,
-          child: ScaleTransition(
-            scale: _scaleAnimation,
-            child: Container(
-              decoration: BoxDecoration(
-                gradient: const LinearGradient(
-                  colors: [
-                    Color(0xFF49280B),
-                    Color(0xFFE4A46B),
-                    Color(0xFF60350F),
-                  ],
-                ),
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withAlpha(77),
-                    blurRadius: 10,
-                    offset: const Offset(0, 5),
-                  ),
-                ],
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(2.0),
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 12,
-                  ),
-                  decoration: BoxDecoration(
-                    color: Colors.black,
-                    borderRadius: BorderRadius.circular(14),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Icon(Icons.info_outline, color: Color(0xFFE4A46B)),
-                      const SizedBox(width: 12),
-                      Flexible(
-                        child: Text(
-                          widget.message,
-                          style: const TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 14,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
           ),
         ),
       ),
